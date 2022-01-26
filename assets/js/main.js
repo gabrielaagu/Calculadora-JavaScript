ef428b43168b63b2af3929c158eefcc4a1d6d857
Gabriela <gabrielaauzcategui@gmail.com>
2022-01-26 15:06:45 +0100

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    const numberButtons = document.getElementById("data-number");
+    const operationButtons = document.getElementById("data-operation");
+    const allClearButton = document.getElementById("data-all-clear");
+    const deleteButton = document.getElementById("data-delete");
+    const equalsButton = document.getElementById("data-equals");
+    const previousOutput = document.getElementById("previous-operand");
+    const currentOutput = document.getElementById("current-operand");
+}
